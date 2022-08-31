c1653e12869bb14971bd29c38dfd6470ed980bf3
fiaui <109265532+fiaui@users.noreply.github.com>
2022-08-31 10:36:54 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="coba_tampil_session.php">
+    id
+    <br>
+    <input type="text">
+    <br>
+    Username
+    <br>
+    <input type="text">
+    <br>
+    <button type="submit">proses</button>
+    </form>
+</body>
+</html>
