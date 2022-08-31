35b7966b27ce7c7378e485b01985746be2603279
fiaui <109265532+fiaui@users.noreply.github.com>
2022-08-31 10:35:39 +0700

    Add files via upload

---
+++ coba_session.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+$_SESSION["id"] = "1234";
+$_SESSION["username"] = "Alifia Rindiani";
+?>
---
+++ coba_tampil_session.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+echo "id = ".$_SESSION['id'];
+echo "<br />";
+echo "username = ".$_SESSION['username'];
+
+?>
